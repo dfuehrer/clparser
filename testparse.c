b8ba6579f1ec6e65649d5ef807a97da81954eba5
dfuehrer <dfuehrer@email.arizona.edu>
2020-04-06 02:43:16 -0700

    started repo. testparse should turn into just a parser for cl args for shell scripts.  basic stuff easily done in awk but poorly written in C.

---
+++ testparse.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUF_SIZE    1000   // yes this will probably cause me problems later, maybe ill even make it dynamic like a quitter
+#define OUTPUT_SIZE 2000   // yes this will probably cause me problems later, maybe ill even make it dynamic like a quitter
+
+// so the idea is that youll send in configuration stuffs over stdin and then have it take in the clargs as clargs and then itll output things that i guess are helpful based on the config stuffs sent in over stdin
+// theres no way that this will be overcomplicated or impossible to use or write at all
+// ill define that this can have flags and options that are set to values and a default field that everything else falls into
+// ill do it in the gnuish i think style where there can be words that use the --, single letters use - and can be combined 
+// if the single letter - options arent flags then the next arguments will be used as the options for that parameter thingy i dont know howw to explain
+// it should go in order of the options it gets so if they contradict then itll be the last option
+// the word parameters can use a space or an = with no spaces
+// the stdin should be of the form:
+// "flags: f,flag g, qwerty h,hist; parameters: q,asdf u nothing zzz,z;"
+// where things that are in the same word separated by commas are the same flag or parameter (if its only 1 char its a single char - else its a -- word)
+// things after flags are flags and the stuff after parameters are the options with values to be set
+// this should return a string like:
+//  f=1
+//  flag=1
+//  g=1
+//  qwerty=1
+//  h=1
+//  hist=1
+//  q=whatever
+//  asdf=whatever
+//  nothing=sure
+//  defaults="who even cares"
+// if called with arguments "-fgq whatever --qwerty --nothing=sure who even cares
+// theoretically im thinking this way you run it like $(echo "..." | parse $@) and itll set the variables accordingly so you can use them without doing any parsing
+int main(int argc, char ** argv){
+    // this is just for debugging
+    for(int i = 1; i < argc; i++){
+        printf("%s\n", argc[i]);
+    }
+    char cbuf[BUF_SIZE];
+    fgets(cbuf, BUF_SIZE, stdin);
+    printf("%s\n", cbuf);
+
+    char flags[BUF_SIZE];
+    char params[BUF_SIZE];
+    char * flagptr = strstr(cbuf, "flags:");
+    char * paramptr = strstr(cbuf, "parameters:");
+    if(flagptr == NULL){
+        memset(flags, 0, BUF_SIZE);
+    }else{
+        strcpy(
+
+    for(int i = 1; i < argc; i++){
+        if(argv[i] == '-'){
+            // this is a single letter flag so loop through all the next letters
+            for(char * f = argv[i][1]; *f; f++){
+                if(!isalnum(*f)){
+                    return 1;
+                }else if(strchr(
+                
+            }
+        }
+    }
+
+    return 0;
+}
